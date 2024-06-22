467875ad5cd7afda0de7f2e32f0df0426c83122a
prabhatsharma9438 <prabhatsharma2630@gmail.com>
2024-06-22 23:30:13 +0530

    Merge branch 'master' of https://github.com/P-R-Technopsych/CareerOn_OES

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +18,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/register',[AuthController::class, 'loadRegister']);
+Route::post('/register',[AuthController::class,'studentRegister'])->name('studentRegister');
